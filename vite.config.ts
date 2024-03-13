3c3d8fba17da717f92e45bafbb0991f702f59d22
thecoderishi <rishirverma512@gmail.com>
2024-03-13 19:28:12 +0530

    iconify installed

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, transformWithEsbuild } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/spectus-website/admin/",
   plugins: [
     {
       name: "treat-js-files-as-jsx",
